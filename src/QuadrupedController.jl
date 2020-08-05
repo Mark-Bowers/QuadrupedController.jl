14767d8d1e6e2604712dfdbddecf2fe26b5609e6
Mark Bowers <mark_bowers@live.com>
2020-08-05 09:44:31 -0700

    Started to more accurately define the Controller struct - added StaticArrays package to the project - changed jointangles type to a static matrix (SMatrix{3, 4, Float64, 12})

--- src/QuadrupedController.jl
+++ src/QuadrupedController.jl
@@ -1,11 +1,17 @@
 module QuadrupedController
 
+using StaticArrays
+
 struct Controller
     swingparams
     stanceparams
     gaitparams
     mvref
-    jointangles
+    jointangles::SMatrix{3, 4, Float64, 12}
+    function Controller()
+
+        new(nothing, nothing, nothing, nothing, zeros(SMatrix{3, 4, Float64}))
+     end
 end
 
 end
