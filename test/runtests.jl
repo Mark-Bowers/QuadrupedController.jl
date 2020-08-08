479d9907fbba52a887568bb4a78f71b38ad26b02
Mark Bowers <mark_bowers@live.com>
2020-08-07 22:13:47 -0700

    Added Controller class

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,26 +5,39 @@ using PyCall
 
 @testset "QuadrupedController.jl" begin
     @test QuadrupedController != PyNULL()
-    @test size(names(QuadrupedController)) == (3,)
+    @test size(names(QuadrupedController)) == (4,)
     @test names(QuadrupedController)[1] == :Configuration
-    @test names(QuadrupedController)[3] == :four_legs_inverse_kinematics
+    @test names(QuadrupedController)[2] == :Controller
+    @test names(QuadrupedController)[3] == :QuadrupedController
+    @test names(QuadrupedController)[4] == :four_legs_inverse_kinematics
 end
 
 @testset "Configuration" begin
-    @test QuadrupedController.Configuration != PyNULL()
-    @test QuadrupedController.Configuration.__module__ == "Config"
-    @test string(QuadrupedController.Configuration) == "PyObject <class 'Config.Configuration'>"
+    @test Configuration != PyNULL()
+    @test Configuration.__module__ == "Config"
+    @test string(Configuration) == "PyObject <class 'Config.Configuration'>"
 
-    config = QuadrupedController.Configuration()
+    config = Configuration()
     @test config.ABDUCTION_OFFSET == 0.03
 end
 
 @testset "four_legs_inverse_kinematics" begin
-    @test QuadrupedController.four_legs_inverse_kinematics != PyNULL()
-    @test QuadrupedController.four_legs_inverse_kinematics.__module__ == "Kinematics"
-    @test string(QuadrupedController.four_legs_inverse_kinematics)[1:47] == "PyObject <function four_legs_inverse_kinematics"
-    @test typeof(methods(QuadrupedController.four_legs_inverse_kinematics).ms[1]) == Method
+    @test four_legs_inverse_kinematics != PyNULL()
+    @test four_legs_inverse_kinematics.__module__ == "Kinematics"
+    @test string(four_legs_inverse_kinematics)[1:47] == "PyObject <function four_legs_inverse_kinematics"
+    @test typeof(methods(four_legs_inverse_kinematics).ms[1]) == Method
 
     np = pyimport("numpy")
-    @test QuadrupedController.four_legs_inverse_kinematics(np.zeros((3,4)), QuadrupedController.Configuration())[2,4] ≈ -0.2627961442139497
+    @test four_legs_inverse_kinematics(np.zeros((3,4)), Configuration())[2,4] ≈ -0.2627961442139497
+end
+
+@testset "Controller" begin
+    @test Controller != PyNULL()
+    @test Controller.__module__ == "Controller"
+    @test string(Controller) == "PyObject <class 'Controller.Controller'>"
+
+    controller = Controller(Configuration(), four_legs_inverse_kinematics,)
+    @test controller.config.ABDUCTION_OFFSET == 0.03
+    @test controller.gait_controller.subphase_ticks(49) == 14
+    @test controller.gait_controller.subphase_ticks(50) == 0
 end
